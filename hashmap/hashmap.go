aa1f2e207a648d3c6392b564b50e443733e32ef4
Marcin Radoszewski <marcin.radoszewski@allegrogroup.com>
2017-04-23 18:48:10 +0000

    Persistent hashmap development

---
+++ hashmap/hashmap.go
@@ -0,0 +1,152 @@
+package hashmap
+
+/**
+ *   Copyright (c) Marcin Radoszewski. All rights reserved.
+ *   The use and distribution terms for this software are covered by the
+ *   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+ *   which can be found in the file epl-v10.html at the root of this distribution.
+ *   By using this software in any fashion, you are agreeing to be bound by
+ * 	 the terms of this license.
+ *   You must not remove this notice, or any other, from this software.
+ **/
+
+import (
+	"errors"
+	"fmt"
+	. "gods"
+)
+
+type Node interface {
+	Copy() Node
+	Assoc(key uint32, val *Value, part uint) Node
+	Find(key uint32, part uint) Node
+}
+
+type ValueNode struct {
+	Key       uint32
+	BaseValue *Value
+}
+
+type SubtreeNode struct {
+	BitMapKey uint32
+	Branches  [32]Node
+}
+
+type HashMap struct {
+	root     SubtreeNode
+	hashFunc func(Value) (uint32, error)
+}
+
+/*/
+** Hash helper functions
+/*/
+
+func popcnt(x uint32) (n byte) {
+	// bit population count, see
+	// http://graphics.stanford.edu/~seander/bithacks.html#CountBitsSetParallel
+	x -= (x >> 1) & 0x55555555
+	x = (x>>2)&0x33333333 + x&0x33333333
+	x += x >> 4
+	x &= 0x0f0f0f0f
+	x *= 0x01010101
+	return byte(x >> 24)
+}
+
+func hashPart(hash uint32, part uint) uint32 {
+	return (hash & (0x1F << (part * 5)) >> (part * 5))
+}
+
+func hashString(s string, level int) uint32 {
+	var vHash, a, b uint32
+	vHash, a, b = 0, 31415, 27183
+	for _, c := range s {
+		vHash = a*vHash*uint32(level) + uint32(c)
+		a *= b
+	}
+	return vHash
+}
+
+func Hash(value Value) (uint32, error) {
+	switch value.(type) {
+	case string:
+		return hashString(value.(string), 1), nil
+	}
+	return 0, errors.New(fmt.Sprintf("Don't know how to hash %v", value))
+}
+
+/*/
+** Value Node Implementation
+/*/
+
+func (vn ValueNode) Copy() Node {
+	return ValueNode{vn.Key, vn.BaseValue}
+}
+
+func (vn ValueNode) Assoc(key uint32, val *Value, part uint) Node {
+	return ValueNode{key, val}
+}
+
+func (vn ValueNode) Find(key uint32, part uint) Node {
+	if key == vn.Key {
+		return vn
+	} else {
+		return nil
+	}
+}
+
+/*/
+** Subtree Node Implementation
+/*/
+
+func (sn SubtreeNode) Copy() Node {
+	return SubtreeNode{sn.BitMapKey, sn.Branches}
+}
+
+func (sn SubtreeNode) Assoc(key uint32, val *Value, part uint) Node {
+	index := hashPart(key, part)
+	node := sn.Branches[index]
+	newNode, _ := sn.Copy().(SubtreeNode)
+	if node == nil {
+		newNode.Branches[index] = ValueNode{key, val}
+		return newNode
+	} else {
+		newNode.Branches[index] = node.Assoc(key, val, part+1)
+		return newNode
+	}
+}
+
+func (sn SubtreeNode) Find(key uint32, part uint) Node {
+	index := hashPart(key, part)
+	node := sn.Branches[index]
+	if node != nil {
+		return node.Find(key, part+1)
+	} else {
+		return nil
+	}
+}
+
+/*/
+** Hash Map Trie
+/*/
+
+func New() *HashMap {
+	hm := HashMap{}
+	hm.hashFunc = Hash
+	return &hm
+}
+
+func (hm *HashMap) Assoc(key Value, val *Value) *HashMap {
+	hash, _ := hm.hashFunc(key)
+	newRoot, _ := hm.root.Assoc(hash, val, 0).(SubtreeNode)
+	return &HashMap{newRoot, Hash}
+}
+
+func (hm *HashMap) Find(key Value) *Value {
+	hash, _ := Hash(key)
+	result := hm.root.Find(hash, 0)
+	if vn, ok := result.(ValueNode); ok {
+		return vn.BaseValue
+	} else {
+		return nil
+	}
+}
---
+++ hashmap/hashmap_test.go
@@ -0,0 +1,145 @@
+package hashmap
+
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	. "gods"
+	"testing"
+)
+
+func TestPopCountFunc(t *testing.T) {
+	var key uint32
+	Convey("Given a key", t, func() {
+		key = 314456588
+		Convey("When counting population", func() {
+			So(popcnt(key), ShouldEqual, 14)
+		})
+	})
+}
+
+func TestGettingHashParts(t *testing.T) {
+	var key uint32
+	var part uint32
+	Convey("Given a hash key", t, func() {
+		key = 314456588
+		Convey("Expect to have proper key parts extracted", func() {
+			Convey("First key part", func() {
+				part = hashPart(key, 0)
+				So(part, ShouldEqual, 12)
+			})
+			Convey("Second key part", func() {
+				part = hashPart(key, 1)
+				So(part, ShouldEqual, 16)
+			})
+			Convey("Third key part", func() {
+				part = hashPart(key, 2)
+				So(part, ShouldEqual, 14)
+			})
+		})
+	})
+}
+
+func TestHashing(t *testing.T) {
+	Convey("Hashing string works as expected", t, func() {
+		hashed, _ := Hash("hello")
+		So(hashed, ShouldEqual, 314456588)
+	})
+
+	Convey("Trying to hash unknown type returns an error", t, func() {
+		_, err := Hash(42)
+		So(err, ShouldNotBeNil)
+	})
+}
+
+func TestValueNodeImplementation(t *testing.T) {
+	var vn ValueNode
+	var copied ValueNode
+	var oldVal Value
+	var newVal Value
+	oldVal = "Hello"
+	newVal = "World"
+	Convey("Given a ValueNode", t, func() {
+		vn = ValueNode{123, &oldVal}
+		Convey("When assoc-ing new value to the node", func() {
+			copied = vn.Assoc(123, &newVal, 0).(ValueNode)
+
+			Convey("Copied node should have the new value", func() {
+				So(copied.Key, ShouldEqual, 123)
+				So(*copied.BaseValue, ShouldEqual, "World")
+			})
+
+			Convey("Original node should not have changed", func() {
+				So(vn.Key, ShouldEqual, 123)
+				So(*vn.BaseValue, ShouldEqual, "Hello")
+			})
+		})
+	})
+}
+
+func TestBasicHashMapFunctionality(t *testing.T) {
+	var emptyMap *HashMap
+	var value Value
+	var value2 Value
+	value = 42
+	value2 = 24
+	Convey("Given an empty map", t, func() {
+		emptyMap = New()
+		Convey("When adding new values", func() {
+			modified := emptyMap.Assoc("myKey", &value)
+			modified2 := modified.Assoc("anotherKey", &value2)
+
+			Convey("The keys should be findable in the map", func() {
+				So(*modified.Find("myKey"), ShouldEqual, 42)
+				So(*modified2.Find("myKey"), ShouldEqual, 42)
+				So(*modified2.Find("anotherKey"), ShouldEqual, 24)
+			})
+
+			Convey("But the value of original maps should not be modified", func() {
+				So(emptyMap.Find("myKey"), ShouldEqual, nil)
+				So(emptyMap.Find("anotherKey"), ShouldEqual, nil)
+				So(modified.Find("anotherKey"), ShouldEqual, nil)
+			})
+		})
+	})
+}
+
+func TestSearchingInEmptyMap(t *testing.T) {
+	var emptyMap *HashMap
+	var value *Value
+	Convey("Given an empty map", t, func() {
+		emptyMap = New()
+		Convey("When searching for nonexistent key in the map", func() {
+			value = emptyMap.Find("not-existing")
+			Convey("Returned value should be nil", func() {
+				So(value, ShouldEqual, nil)
+			})
+		})
+	})
+}
+
+func TestOverridingAValue(t *testing.T) {
+	var first *HashMap
+	var second *HashMap
+	var value Value
+	var newValue Value
+	value = 42
+	newValue = 24
+
+	Convey("Given a map with a value", t, func() {
+		first = New().Assoc("key", &value)
+		Convey("When value is overriden", func() {
+			second = first.Assoc("key", &newValue)
+			Convey("The new value should be returned", func() {
+				So(second.Find("key"), ShouldEqual, &newValue)
+			})
+			Convey("But the original map should remain unchanged", func() {
+				So(first.Find("key"), ShouldEqual, &value)
+			})
+		})
+	})
+
+}
+
+//func TestCollidingHashes(t *testing.T) {
+//	hash := func(val Value) (uint32, error) {
+//	}
+//}
